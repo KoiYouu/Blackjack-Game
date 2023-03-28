d5b3b502e371ed6800d7da6a7f6e52f9afaa9711
Eric <ericxiang8@hotmail.com>
2023-03-27 17:50:56 -0700

    added save, load, and view scoreboard gui

--- src/main/ui/BlackjackGUI.java
+++ src/main/ui/BlackjackGUI.java
@@ -1,13 +1,16 @@
 package ui;
 
 import model.Dealer;
+import model.Gambler;
 import model.ListOfGamblers;
 import persistence.JsonReader;
 import persistence.JsonWriter;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
-import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class BlackjackGUI extends JPanel {
     private JPanel guiCardLayout;
@@ -33,7 +36,6 @@ public class BlackjackGUI extends JPanel {
     private JPanel addPlayerJPanel;
     private JPanel addBalanceJPanel;
     private JLabel scoreboardPlayerLabel;
-    private JLabel scoreboardWinLabel;
     private JLabel scoreboardLossesLabel;
     private JLabel scoreboardDrawsLabel;
     private JLabel scoreboardBalanceLabel;
@@ -47,6 +49,8 @@ public class BlackjackGUI extends JPanel {
     private JPanel startingMoneyJPanel;
     private JLabel numPlayerLabel;
     private JLabel startingMoneyLabel;
+    private JLabel scoreboardWinsLabel;
+    private JButton scoreboardBackToMenuButton;
 
 
     private Dealer dealer;
@@ -65,6 +69,8 @@ public class BlackjackGUI extends JPanel {
             public void actionPerformed(ActionEvent e) {
                 listOfGamblers = new ListOfGamblers(numPlayerSelectBox.getSelectedIndex() + 1,
                         Integer.parseInt(startingMoneyTextField.getText()));
+                jsonWriter = new JsonWriter(SAVELOCATION);
+                jsonReader = new JsonReader(SAVELOCATION);
                 guiCardLayout.removeAll();
                 guiCardLayout.add(gameMenuCardLayout);
                 guiCardLayout.repaint();
@@ -81,20 +87,102 @@ public class BlackjackGUI extends JPanel {
                 }
             }
         });
-        numPlayerSelectBox.addItemListener(new ItemListener() {
+        saveGameButton.addActionListener(new ActionListener() {
             @Override
-            public void itemStateChanged(ItemEvent e) {
-
+            public void actionPerformed(ActionEvent e) {
+                saveGameData();
+                JOptionPane.showMessageDialog(null, "Saved game data to " + SAVELOCATION,
+                        "Saved Successfully", JOptionPane.INFORMATION_MESSAGE);
             }
         });
+        loadSGameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loadGameData();
+                JOptionPane.showMessageDialog(null, "Loaded game data from " + SAVELOCATION,
+                        "Loaded Successfully", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+        viewScoreboardButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                guiCardLayout.removeAll();
+                guiCardLayout.add(scoreboardJPanel);
+                guiCardLayout.repaint();
+                guiCardLayout.revalidate();
+                addGamblerData();
+            }
+        });
+        scoreboardBackToMenuButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                guiCardLayout.removeAll();
+                guiCardLayout.add(gameMenuCardLayout);
+                guiCardLayout.repaint();
+                guiCardLayout.revalidate();
+            }
+        });
+    }
+
+    private void addGamblerData() {
+        scoreboardPlayerJPanel.setLayout(new BoxLayout(scoreboardPlayerJPanel, BoxLayout.Y_AXIS));
+        scoreboardWinsJPanel.setLayout(new BoxLayout(scoreboardWinsJPanel, BoxLayout.Y_AXIS));
+        scoreboardLossesJPanel.setLayout(new BoxLayout(scoreboardLossesJPanel, BoxLayout.Y_AXIS));
+        scoreboardDrawsJPanel.setLayout(new BoxLayout(scoreboardDrawsJPanel, BoxLayout.Y_AXIS));
+        scoreboardBalanceJPanel.setLayout(new BoxLayout(scoreboardBalanceJPanel, BoxLayout.Y_AXIS));
+        scoreboardPlayerJPanel.removeAll();
+        scoreboardWinsJPanel.removeAll();
+        scoreboardLossesJPanel.removeAll();
+        scoreboardDrawsJPanel.removeAll();
+        scoreboardBalanceJPanel.removeAll();
+        for (Gambler gambler: listOfGamblers.getGamblers()) {
+            scoreboardPlayerJPanel.add(new JLabel("Player " + gambler.getGamblerID(),
+                    SwingConstants.CENTER));
+            scoreboardWinsJPanel.add(new JLabel(Integer.toString(gambler.getWins()),
+                    SwingConstants.CENTER));
+            scoreboardLossesJPanel.add(new JLabel(Integer.toString(gambler.getLosses(),
+                    SwingConstants.CENTER)));
+            scoreboardDrawsJPanel.add(new JLabel(Integer.toString(gambler.getDraws(),
+                    SwingConstants.CENTER)));
+            scoreboardBalanceJPanel.add(new JLabel(Integer.toString(gambler.getBalance(),
+                    SwingConstants.CENTER)));
+        }
+    }
+
+
+    // EFFECTS: saves the gambler data to file
+    private void saveGameData() {
+        try {
+            jsonWriter.open();
+            jsonWriter.write(listOfGamblers);
+            jsonWriter.close();
+
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Could not save",
+                    "Failed to save", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: loads gambler data from file
+    private void loadGameData() {
+        try {
+            listOfGamblers = jsonReader.read();
+
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Could not load",
+                    "Failed to save", JOptionPane.INFORMATION_MESSAGE);
+        }
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("BlackjackGUI");
         frame.setContentPane(new BlackjackGUI().guiCardLayout);
+        frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setResizable(false);
+        frame.setResizable(true);
         frame.setVisible(true);
+
     }
 }
